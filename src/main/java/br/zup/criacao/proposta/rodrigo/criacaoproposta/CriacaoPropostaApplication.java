b565db21291c9929d99913cf0ca98ee5e3136c8e
Rodrigo Ananias <rodrigo.ananias@zup.com.br>
2021-06-03 13:31:43 -0300

    setup do projeto

---
+++ src/main/java/br/zup/criacao/proposta/rodrigo/criacaoproposta/CriacaoPropostaApplication.java
@@ -0,0 +1,13 @@
+package br.zup.criacao.proposta.rodrigo.criacaoproposta;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CriacaoPropostaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CriacaoPropostaApplication.class, args);
+	}
+
+}
